16e32d8933756b358c281c1b2a9bd2312be79f6a
prajwallanjewar01 <prajwallanjewar442905@gmail.com>
2025-05-18 23:47:27 +0530

    Added all files in git folder - 18 May 2025 on 11:46 PM

---
+++ app.js
@@ -0,0 +1,37 @@
+let gameSeq = []
+let userSeq = []
+let btns = ["color1", "color2", "color3", "color4"];
+
+let started = false;
+let level = 0;
+
+let h2 = document.querySelector("h2");
+
+document.addEventListener("keypress", function () {
+    if (started == false) {
+        console.log("Game Stated");
+        started = true;
+        h2.innerText = `Game Started`;
+    }
+
+});
+
+function btnFlash(btn) {
+    btn.classList.add("flash");
+    setTimeout(function () {
+        btn.classList.remove("flash");
+    }, 1000);
+}
+
+function levelUp() {
+    level++;
+    h2.innerText = `Level ${level}`;
+
+    let randIdx = Math.floor(Math.random() * 3);
+    let randColor = btns[randIdx];
+    let randBtn = document.querySelector(`.${randColor}`);
+    console.log("randIdx : ",randIdx);
+    console.log("randBtn : ",randBtn);
+    console.log("randColor : ",randColor);
+    btnFlash(randBtn);
+}
